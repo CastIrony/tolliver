01c54e1175435ebb202e8f2ec08fa5f75dc75e6b
Joel Bernstein <bergamot@gmail.com>
2016-07-10 21:22:14 -0700

    Updated setup.py again

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'tolliver',
   packages = ['tolliver'],
-  version = '0.1',
+  version = '0.1a',
   description = 'An application for dialing in CNC hole tolerances',
   author = 'CastIrony',
   author_email = 'bergamot@gmail.com',
   url = 'https://github.com/castirony/tolliver',
-  download_url = 'https://github.com/castirony/tolliver/tarball/0.1',
+  download_url = 'https://github.com/castirony/tolliver/tarball/0.1a',
   keywords = ['cnc', 'tolerance'],
-  classifiers = [],
+  requires = ['docopt', 'cerberus', 'ezdxf'],    
 )
