d2b3696b997d3851e263beb3da92313555929606
Joel Bernstein <bergamot@gmail.com>
2016-07-10 21:12:55 -0700

    Updated setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'CastIrony',
   author_email = 'bergamot@gmail.com',
   url = 'https://github.com/castirony/tolliver',
-  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1',
+  download_url = 'https://github.com/castirony/tolliver/tarball/0.1',
   keywords = ['cnc', 'tolerance'],
   classifiers = [],
 )
